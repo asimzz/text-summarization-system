2bbe612abb271b4741ab2df3202a04e89e8aa1b6
Asim Abdalla <asimabdalla99@gmail.com>
2024-10-04 21:33:13 +0000

    change schemas directory

---
+++ schemas/__init__.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+from typing import List
+
+class UserCredentials(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class Texts(BaseModel):
+    texts: List[str]
+
+
+class User(BaseModel):
+    username: str
+    full_name: str
+    password: str
+    email: str
+    role: str
+    created_at: str
+
+
+class RequestLog(BaseModel):
+    username: str
+    user_id: str
+    time: str
+    endpoint: str
+    status_code: int
+    request_body: dict
+    request_headers: dict
