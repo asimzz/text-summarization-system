2fc1337403de14c77c845f6b888b6e230ae45d39
Asim Abdalla <asimabdalla99@gmail.com>
2024-10-14 00:02:03 +0000

    add test shell script

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+# Function to delete __pycache__ folders
+cleanup() {
+    find . -type d -name "__pycache__" -exec rm -r {} +
+}
+
+# Trap the EXIT signal to run the cleanup function when the script exits
+trap cleanup EXIT
+
+pytest --disable-warnings tests/test_app.py
