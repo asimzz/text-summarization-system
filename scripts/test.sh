484b6fc4e6f74466cd0ae7e9826a529f33722976
Asim Abdalla <asimabdalla99@gmail.com>
2024-10-14 18:04:30 +0000

    update test shell script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 # Function to delete __pycache__ folders
 cleanup() {
     find . -type d -name "__pycache__" -exec rm -r {} +
+    find . -type d -name ".pytest_cache" -exec rm -r {} +
 }
 
 # Trap the EXIT signal to run the cleanup function when the script exits
