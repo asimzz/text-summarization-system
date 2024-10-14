ea40e86e8559ca3bf9d40dbd1f8f7e8ee5ecfade
Asim Abdalla <asimabdalla99@gmail.com>
2024-10-14 18:04:20 +0000

    add logging to db functionality

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Request, Response
 from model import summarizer
 from dotenv import load_dotenv
-from schemas import UserCredentials, User, Text
-from app.auth import generate_token_response, JWTBearer
+from schemas import UserCredentials, User, Text, RequestLog
+from app.auth import generate_token_response, get_authenticated_user
+from app.auth import JWTBearer
 from utils import verify_password
 from db import connect_to_mongo, get_collection
 from datetime import datetime
@@ -20,6 +21,8 @@ app = FastAPI(
 )
 
 db = connect_to_mongo()
+users_collection = get_collection(db, "users")
+logs_collection = get_collection(db, "logs")
 
 
 @app.get("/")
@@ -40,11 +43,9 @@ async def register(user: User):
     Returns:
         dict: A dictionary containing the user's registration details.
     """
-
-    collection = get_collection(db, "users")
     user.created_at = datetime.now()
     user.password = hash_password(user.password)
-    result = collection.insert_one(dict(user))
+    result = users_collection.insert_one(dict(user))
     if not result.inserted_id:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -67,8 +68,7 @@ async def login(credentials: UserCredentials):
     Returns:
         dict: A dictionary containing the access token.
     """
-    collection = get_collection(db, "users")
-    user = collection.find_one({"username": credentials.username})
+    user = users_collection.find_one({"username": credentials.username})
     matched = verify_password(credentials.password, user["password"])
     if not user or not matched:
         raise HTTPException(
@@ -80,9 +80,7 @@ async def login(credentials: UserCredentials):
 
 
 @app.post("/summarize", dependencies=[Depends(JWTBearer())])
-async def summarize_text(
-    originalTexts: Text,
-):
+async def summarize_text(originalTexts: Text, request: Request):
     """
     Summarizes a given text.
     Args:
@@ -91,7 +89,29 @@ async def summarize_text(
     Returns:
         dict: A dictionary containing the summarized text.
     """
+    username = get_authenticated_user(request)
+    user = User.model_validate(users_collection.find_one({"username": username}))
+
     summary = summarizer(
         originalTexts.text, max_length=1000, min_length=30, do_sample=False
     )
-    return {"summary": summary}
+
+    summary_result = {"summary": summary}
+
+    request_log = RequestLog(
+        username=user.username,
+        user_id=str(user._id),
+        time=str(datetime.now()),
+        endpoint=request.url.path,
+        request_body=dict(originalTexts),
+        request_headers=dict(request.headers),
+        response_body=dict(summary_result),
+    )
+
+    log_result = logs_collection.insert_one(dict(request_log))
+    if not log_result.inserted_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User registration failed",
+        )
+    return summary_result
